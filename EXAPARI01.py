727b231e532af41680f718d34705b31b123d664e
Cris1107FC <cristian0711fc@gmail.com>
2022-06-22 13:40:05 -0500

    EXAMEN PRIMER PARCIAL

---
+++ EXAPARI01.py
@@ -0,0 +1,19 @@
+print("EXAMEN PRIMER PARCIAL - Cristian Fuentes Ceron")
+print("Hola profe maik !")
+print("numero 1")
+num1=int(input())
+print("numero 2")
+num2=int(input())
+incremento=1 
+if num1 > num2:
+    while incremento <= num1:
+        num2 = num2 + incremento
+        incremento +=1
+        print(num2)
+if num2 > num1:
+    while incremento <= num1:
+        num1 = num1 + incremento
+        incremento *=1
+        print(num1)
+
+     
