dd1121a836aaceeb1df0c8bc2183261e4c9c136a
Cris1107FC <cristian0711fc@gmail.com>
2022-06-22 21:46:54 -0500

    Examen agregado

--- EXAPARI01.py
+++ EXAPARI01.py
@@ -1,19 +1,19 @@
 print("EXAMEN PRIMER PARCIAL - Cristian Fuentes Ceron")
 print("Hola profe maik !")
-print("numero 1")
-num1=int(input())
-print("numero 2")
-num2=int(input())
-incremento=1 
-if num1 > num2:
-    while incremento <= num1:
-        num2 = num2 + incremento
+print("Recuerda si A > B se hara una suma y su B > A se hara una multiplicacion")
+numeroA=int(input("Digite el numero 1"))
+numeroB=int(input("Digite el numero 2"))
+incremento = 1 
+if numeroA > numeroB:
+    while incremento <= numeroA:
+        numeroB = numeroB + incremento
         incremento +=1
-        print(num2)
-if num2 > num1:
-    while incremento <= num1:
-        num1 = num1 + incremento
-        incremento *=1
-        print(num1)
+        print("la suma de las series es:",numeroB)
 
-     
+else:
+    while incremento <= numeroB:
+        numeroA = numeroA * incremento
+        incremento +=1
+        print("La multiplcacion es ",numeroA)
+
+
